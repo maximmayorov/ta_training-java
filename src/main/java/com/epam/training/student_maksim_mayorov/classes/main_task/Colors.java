d2aaea098ab8f997796adfc59e81541aa83c861f
maximmayorov <maj-maksim@yandex.ru>
2021-12-19 21:20:51 +0300

    added Main Task from Java Classes module

---
+++ src/main/java/com/epam/training/student_maksim_mayorov/classes/main_task/Colors.java
@@ -0,0 +1,5 @@
+package com.epam.training.student_maksim_mayorov.classes.main_task;
+
+public enum Colors {
+    RED, WHITE, BLACK, YELLOW, BLUE, GREEN, GRAY
+}
---
+++ src/main/java/com/epam/training/student_maksim_mayorov/classes/main_task/Runner.java
@@ -0,0 +1,69 @@
+package com.epam.training.student_maksim_mayorov.classes.main_task;
+
+import com.epam.training.student_maksim_mayorov.classes.main_task.Car;
+import com.epam.training.student_maksim_mayorov.classes.main_task.Colors;
+
+import java.util.Calendar;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+//        Создать классы, спецификации которых приведены ниже. Определить конструкторы и методы setТип(), getТип(), toString().
+//        Определить дополнительно методы в классе, создающем массив объектов. Задать критерий выбора данных и вывести эти данные на консоль.
+//        В каждом классе, обладающем информацией, должно быть объявлено несколько конструкторов.
+
+//        8. Car: id, Марка, Модель, Год выпуска, Цвет, Цена, Регистрационный номер.
+//           Создать массив объектов. Вывести:
+//        a) список автомобилей заданной марки;
+//        b) список автомобилей заданной модели, которые эксплуатируются больше n лет;
+//        c) список автомобилей заданного года выпуска, цена которых больше указанной.
+
+        Car[] cars = {
+                new Car(1, "Skoda", "Octavia", 2007, Colors.GRAY, 5_500, "AB123CD"),
+                new Car(2, "BMW", "M5", 2018, Colors.BLACK, 68_750, "EF456GH"),
+                new Car(3, "Mercedes-Benz", "W124", 1992, Colors.BLACK, 2_748, "ML125RD"),
+                new Car(4, "Toyota", "XV30", 2003, Colors.WHITE, 6_000, "AA777BB"),
+                new Car(5, "BMW", "X5", 2008, Colors.GRAY, 17_862, "CD459WW"),
+                new Car(6, "Toyota", "Mark 2", 2003, Colors.WHITE, 7_725, "AB129XZ"),
+                new Car(7, "BMW", "E39", 2003, Colors.BLUE, 6_100, "DD412GH"),
+                new Car(8, "Mercedes-Benz AMG", "GT 43", 2019, Colors.YELLOW, 115_189, "MB521RF"),
+                new Car(9, "Volkswagen", "Passat B6 ", 2009, Colors.GREEN, 5_692, "EE788DR"),
+                new Car(10, "BMW", "E39", 2001, Colors.RED, 4_608, "TR479LL"),
+        };
+
+        System.out.println("BMW cars:");
+        carsOfBrand(cars, "BMW");
+        System.out.println("\nBMW E39 model cars that have been in use for more than 7 years:");
+        carsOfModelOperatedMoreThanYears(cars, "E39", 18);
+        System.out.println("\nCars of 2003 year of manufacture costing more than $6000:");
+        carsOfYearAndPrice(cars, 2003, 6000);
+
+    }
+
+    public static void carsOfBrand(Car[] cars, String brand) {
+        for (Car car : cars) {
+            if (car.getBrand().equalsIgnoreCase(brand)) {
+                System.out.println(car);
+            }
+        }
+    }
+
+    public static void carsOfModelOperatedMoreThanYears (Car[] cars, String model, int nYears) {
+        for (Car car : cars) {
+            if ((Calendar.getInstance().get(Calendar.YEAR) - car.getYear()) > nYears
+                    && car.getModel().equals(model)) {
+                System.out.println(car);
+            }
+        }
+    }
+
+    public static void carsOfYearAndPrice(Car[] cars, int year, int price) {
+        for (Car car : cars) {
+            if (car.getYear() == year && car.getPrice() > price) {
+                System.out.println(car);
+            }
+        }
+    }
+
+}
